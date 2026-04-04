25dc1d28ae9da999b9f016de61c742f36b6dbe71
z4trox <kylian.lefebvre60@gmail.com>
2026-04-04 10:03:44 +0200

    Add Electron main process

---
+++ electron/main.cjs
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: require('path').join(__dirname, 'preload.cjs')
+    }
+  });
+
+  win.loadURL('https://novelai.net');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
